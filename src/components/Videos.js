ab03b2e3f6bed5c2abf42ab621ebe784572c86f4
Md. Khasrur Rahman <30666985+KhasrurRahman@users.noreply.github.com>
2022-02-02 14:48:45 +0600

    initial app

---
+++ src/components/Videos.js
@@ -0,0 +1,17 @@
+import classes from '../style/videos.module.css'
+import Video from './pages/Video'
+export default function Videos()
+{
+    return (
+        <div className={classes.videos}>
+            <Video/>
+            <Video/>
+            <Video/>
+            <Video/>
+            <Video/>
+            <Video/>
+            <Video/>
+            <Video/>
+        </div>
+    )
+}
